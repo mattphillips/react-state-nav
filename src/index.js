4dc2f7fcb54edaf20af4b96a206bc7b437016e43
Matt Phillips <mattphillips1016@gmail.com>
2016-10-06 23:45:18 +0100

    Add basic event loop with state and navigation handling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,49 @@ import { render } from 'react-dom';
 import App from './components/App';
 import './index.css';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  currentPage: 'one',
+  values: {
+    one: 'page one',
+    two: 'page two',
+    three: 'page three'
+  },
+  routes: {
+    one: {
+      forward: 'two',
+      back: null
+    },
+    two: {
+      forward: 'three',
+      back: 'one'
+    },
+    three: {
+      forward: null,
+      back: 'two'
+    }
+  }
+};
+
+const updateState = newState => {
+  window.onpopstate = e => {
+    updateState({
+      ...newState,
+      currentPage: e.state
+    });
+  };
+
+  console.log('update state', newState);
+
+  render(
+    <App state={newState} update={updateState}/>,
+    document.getElementById('root')
+  );
+};
+
+window.onload = () => {
+  if (history.state === null) {
+    window.history.replaceState(initialState.currentPage, null, null);
+  }
+};
+
+updateState(initialState);
