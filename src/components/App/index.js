0795f0dd4798fae61d998dd6a87915a9264f62ac
Matt Phillips <mattphillips1016@gmail.com>
2016-10-06 23:45:51 +0100

    Add inputs to change page values and navigate

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './App.css';
+import Page from '../Page';
 
-export default function App () {
+const click = (update, newPage, state) => (e) => {
+  window.history.pushState(newPage, null, newPage);
+  update({
+    ...state,
+    currentPage: newPage
+  });
+};
+
+const change = (update, state) => (e) => {
+  const value = e.target.value;
+  update({
+    ...state,
+    values: {
+      ...state.values,
+      [state.currentPage]: value
+    }
+  });
+};
+
+export default function App ({ state, update }) {
+  const page = state.currentPage;
+  const { routes, values } = state;
   return (
     <div className="App">
-      <h1>Hello, world</h1>
+      <Page>
+        <input onChange={change(update, state)} type="text" value={values[page]} />
+        {
+          page !== 'one' &&
+          <button type="button" onClick={click(update, routes[page].back, state)}>Back</button>
+        }
+        {
+          page !== 'three' &&
+          <button type="button" onClick={click(update, routes[page].forward, state)}>Forward</button>
+        }
+      </Page>
     </div>
   );
 }
